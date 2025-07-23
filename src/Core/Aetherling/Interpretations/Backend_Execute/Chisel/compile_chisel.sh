768af4c3e170b1875aec363de14bfadf879debe6
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2025-07-23 15:37:08 -0400

    Fix bugs in Chisel backend

--- src/Core/Aetherling/Interpretations/Backend_Execute/Chisel/compile_chisel.sh
+++ src/Core/Aetherling/Interpretations/Backend_Execute/Chisel/compile_chisel.sh
@@ -6,6 +6,8 @@ echo ""
 set -o xtrace
 cp "$1" "${2}/src/main/scala/aetherling/modules/Top.scala"
 cd "$2"
+# If compilation fails, don't just copy over the previous Verilog code
+rm -f "$2/Top.v"
 sbt -mem 2048 "runMain aetherling.modules.Top"
 cd -
 cp "$2/Top.v" "$3"
